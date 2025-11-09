0996cca8de136fec6e77ca0d602eee928a25a586
sgm27 <sondaitai27@gmail.com>
2025-11-09 08:42:30 +0700

    first commit

---
+++ src/main/java/com/mycompany/demogiaodien/DemoGiaoDien.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demogiaodien;
+
+/**
+ *
+ * @author ngoso
+ */
+public class DemoGiaoDien {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
